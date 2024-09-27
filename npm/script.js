0c55877888cb551940c7d59c5167e2c465e2e0af
Spencer555 <46141953+Spencer555@users.noreply.github.com>
2024-09-27 17:43:32 +0000

    added react

---
+++ npm/script.js
@@ -0,0 +1,20 @@
+var _ = require('lodash')
+var array = [1, 2, 3, 4, 5, 6, 7, 8]
+
+console.log("answer:", _.without(array, 3))
+var color1 = document.getElementById('color1')
+var color2 = document.getElementById('color2')
+var h3 = document.querySelector('h3')
+var body = document.getElementById('gradient')
+
+
+function setGradient() {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    h3.textContent = `Linear Gradient to Right Left: ${color1.value} Right: ${color2.value}`
+}
+
+
+
+
+color1.addEventListener('input', setGradient)
+color2.addEventListener('input', setGradient)
